2540a39bab4a514b103585170ceb2e115c8c99c4
Harshit Kalra <hkalra0001@gmail.com>
2025-08-06 23:42:54 +0530

    Updating Java - Introduction

---
+++ Java/Introduction/Java_Loops_I.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Java_Loops_I{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for ( int i = 1 ; i <= 10 ; i++ ){
+            System.out.println(N+" x "+i+" = "+(N*i));
+        }
+        scanner.close();
+    }
+}\
